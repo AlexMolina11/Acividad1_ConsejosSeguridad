a3d06d3308522af3bd7f0571cc6d2e0a00f9e5d7
kevin <kevinhl082@gmail.com>
2026-05-03 07:49:26 -0500

    Feat: Se agrega navehacion a detalle

--- app/src/main/java/com/example/consejosseguridad/ui/navegacion/NavegacionApp.kt
+++ app/src/main/java/com/example/consejosseguridad/ui/navegacion/NavegacionApp.kt
@@ -1,7 +1,10 @@
 package com.example.consejosseguridad.ui.navegacion
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.*
+import androidx.navigation.navArgument
+import com.example.consejosseguridad.ui.pantallas.detalle.PantallaDetalle
 import com.example.consejosseguridad.ui.pantallas.inicio.PantallaInicio
 
 /* Maneja la navegación de la app. */
@@ -17,8 +20,27 @@ fun NavegacionApp() {
 
         composable("inicio") {
             PantallaInicio(
-                onClickTema = {
-                    // Luego navegaremos a detalle
+                onClickTema = { temaId ->
+                    navController.navigate("detalle/$temaId")
+                }
+            )
+        }
+
+        composable(
+            route = "detalle/{temaId}",
+            arguments = listOf(
+                navArgument("temaId") {
+                    type = NavType.IntType
+                }
+            )
+        ) { backStackEntry ->
+
+            val temaId = backStackEntry.arguments?.getInt("temaId") ?: 0
+
+            PantallaDetalle(
+                temaId = temaId,
+                onVolver = {
+                    navController.popBackStack()
                 }
             )
         }
